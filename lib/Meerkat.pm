407d5c82027b8a9403ce24478202a87b0c8f00bd
David Golden <dagolden@cpan.org>
2013-09-12 20:54:06 -0400

    more documentation

--- lib/Meerkat.pm
+++ lib/Meerkat.pm
@@ -24,7 +24,7 @@ use namespace::autoclean;
 =attr namespace (required)
 
 A perl module namespace that will be prepended to class names requested
-via C<collection>.  If C<namespace> is "Foo::Bar", then
+via the L</collection> method.  If C<namespace> is "Foo::Bar", then
 C<< $meerkat->collection("Baz") >> will load and associate the
 C<Foo::Bar::Baz> class in the returned collection object.
 
@@ -38,7 +38,7 @@ has namespace => (
 
 =attr database_name (required)
 
-A valid MongoDB database used to store all collections generated via the
+A MongoDB database name used to store all collections generated via the
 Meerkat object and its collections factories.  Unless a C<db_name> is provided
 in the C<client_options> attribute, this database will be used instead for any
 authentication.
@@ -53,7 +53,7 @@ has database_name => (
 
 =attr client_options
 
-A hash reference of L<MongoDB::MongoClient> options to pass to the
+A hash reference of L<MongoDB::MongoClient> options that will be passed to its
 C<connect> method.
 
 =cut
@@ -232,8 +232,9 @@ with the result.
 Meerkat is not an object-relational mapper.  It does not offer or manage relations
 or support embedded objects.
 
-Meerkat is fork-safe.  It manage a cache of connections that gets cleared when
-a fork occurs.  It will transparently reconnect from child processes.
+Meerkat is fork-safe.  It manage a cache of MongoDB::Collection objects that
+gets cleared when a fork occurs.  Meerkat will transparently reconnect from
+child processes.
 
 =usage
 
@@ -241,17 +242,18 @@ Meerkat divides functional responsibilities across four classes:
 
 =for :list
 * L<Meerkat> — associates a Perl namespace to a MongoDB connection and database
-* L<Meerkat::Collection> — associates a Perl class to a MongoDB collection
-* L<Meerkat::Role::Document> -- enhances a Moose object with Meerkat methods and metadata
-* L<Meerkat::Cursor> — proxies a result cursor and inflates results into objects
+* L<Meerkat::Collection> — associates a Perl class within a namespace to a MongoDB collection
+* L<Meerkat::Role::Document> — enhances a Moose object with Meerkat methods and metadata
+* L<Meerkat::Cursor> — proxies a result cursor and inflates documents into objects
 
-You define your documents as Moose classes that consumes the L<Meerkat::Role::Document>
+You define your documents as Moose classes that consumes the Meerkat::Role::Document
 role.  This gives them several support methods to update, synchronize or remove
 themselves from the database.
 
 In order to create objects from your model or retrieve them from the database,
 you must first create a Meerkat object that manages your connection to the
-MongoDB database.
+MongoDB database.  This is where you specify your database host, authentication
+options and so on.
 
 You then get a Meerkat::Collection object from the Meerkat object, which holds
 an association between the model class and a collection in the database.  This
@@ -274,7 +276,7 @@ or by not catching errors thrown by MongoDB classes.
 Your objects are subject to the same limitations as any MongoDB document.
 
 Most significantly, because MongoDB uses the dot character as a field separator
-in queries (C<foo.bar>, you may not have the dot character as the key of any
+in queries (e.g. C<foo.bar>), you may not have the dot character as the key of any
 hash in your document.
 
     # this will fail
@@ -284,7 +286,54 @@ Be particularly careful with email addresses and URLs.
 
 =head1 RATIONALE
 
-...
+Working with raw MongoDB documents as pure data structures is a bit painful and
+annoying.  There are some existing libraries that attempt to make life easier,
+but I found them deficient in one way or another.
+
+I tried L<Mongoose> first.  I had problems when trying to work with multiple
+databases and doing any sort of authentication and it doesn't seem very
+actively maintained.  L<MongoDBX::Class> (discussed next) has some L<additional
+Mongoose critiques|MongoDBx::Class/COMPARISON WITH OTHER MongoDB ORMs>.
+Mongoose is about 1000 lines of code split across fourteen modules.
+
+Next I looked at L<MongoDBx::Class>.  In many ways, it works much more like the
+L<MongoDB> classes.  What stopped me cold was that it requires inserts to be
+done with a raw data structure.  That means no defaults, validation, lazy
+building and other stuff that I like about Moose.  It does offer some support
+making updates easier, and I've adapted that approach for Meerkat.
+MongoDBx::Class is about 800 lines of code split across fifteen modules.
+
+Both offer a relational model.  While a noble goal, I'm suspicious of applying
+relational data models to a document-oriented database like MongoDB that
+doesn't have transactions.  MongoDB offers atomic I<document> updates, so I
+decided to focus Meerkat on that alone.
+
+Mongoose and MongoDBx also support defining embedded documents.  I haven't
+decided if that's necessary, so I haven't implemented it in Meerkat.
+
+There are other MongoDB things out there that I found and dismissed:
+
+=for :list
+* L<KiokuDB::Backend::MongoDB>, but see the Mongoose L<critique of it|Mongoose::Intro/MOTIVATION>
+* L<MongoDB::Simple>, which is too simple to do what I want
+* L<MongoDBx::Tiny>, which hurts my eyes
+* L<MongoDBI>, "scheduled for a rewrite in the coming months" for the last year
+
+Conceptually, Meerkat is a bit similar to Mongoose, but less ambitious.  (A
+meerkat is a smaller member of the mongoose family, after all.)  It adopts
+some of the features I liked from MongoDBx::Class.
+
+Meerkat focuses on:
+
+=for :list
+* Multiple database support
+* Easy configuration of database connections
+* Fork safety
+* Simplicity and (to the extent possible) Moosey-ness
+* A document-centric data model
+
+Because it's less ambitious, Meerkat is smaller and less complex, currently
+about 400 lines of code split across four modules.
 
 =head1 SEE ALSO
 
